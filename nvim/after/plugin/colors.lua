f7315a5b6104e42989406b559a5fad5cd88f42e6
Keivan-sf <keyvan0082@gmail.com>
2023-10-24 16:26:56 +0330

    Update nvim color theme config

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,23 +1,25 @@
 require("github-theme").setup({
-    options = {
-        transparent = true,
-    }
+	options = {
+		-- transparent = false,
+	},
 })
 
 function ColorizeVim(color)
 	color = color or "github_dark_tritanopia"
 	vim.cmd.colorscheme(color)
-    vim.cmd("highlight Normal guibg=none")
-    vim.cmd("highlight NonText guibg=none")
-    vim.cmd("highlight Normal ctermbg=none")
-    vim.cmd("highlight NonText ctermbg=none")
+
+	-- These commands work like crazy!
+	-- vim.cmd("highlight Normal guibg=none")
+	-- vim.cmd("highlight NonText guibg=none")
+	-- vim.cmd("highlight Normal ctermbg=none")
+	-- vim.cmd("highlight NonText ctermbg=none")
+
+	-- the following did not work
 	-- vim.api.nvim_set_hl(0 , "Nomral" , {bg = "none"})
 	-- vim.api.nvim_set_hl(1 , "NomralFloat" , {bg = "none"})
 end
-
 
 -- ColorizeVim("github_dark")
 -- ColorizeVim("github_dark_dimmed")
 -- ColorizeVim("rose-pine")
 ColorizeVim()
-
